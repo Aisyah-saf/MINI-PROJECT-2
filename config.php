5a83beb2b70b32db9d297328eb3ae6b82468274e
Adlyn Husaini <adlynwork1508@gmail.com>
2026-04-20 11:23:58 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,13 @@
 <?php
-session_start(); 
+session_start();
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db   = "assignment_system";
 
-$conn = new mysqli("localhost","root","","assignment_system");
+$conn = new mysqli($host, $user, $pass, $db);
 
-if($conn->connect_error){
-    die("Connection failed");
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
 ?>
