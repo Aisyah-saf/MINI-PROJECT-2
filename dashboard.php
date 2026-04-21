96ff310763de3ff44563a635bc274802137a19e6
Adlyn Husaini <adlynwork1508@gmail.com>
2026-04-21 23:15:01 +0800

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -1,18 +1,28 @@
-<?php include 'header.php'; ?>
-
-<h3 class="text-center mt-4">Welcome <?= $_SESSION['name']; ?></h3>
-<hr class="w-25 mx-auto">
-
-<div class="card-box col-md-6 mx-auto">
-
-<input type="text" placeholder="Search assignments..." class="form-control search-box">
+<?php 
+include 'header.php'; 
+?>
 
 <?php
-$res=$conn->query("SELECT * FROM assignments");
+if(!isset($_SESSION['user_id'])){
+    header("Location: login.php");
+    exit();
+}
+?>
 
-if($res->num_rows>0){
-    while($row=$res->fetch_assoc()){
-        echo "<div class='assignment-item'>{$row['title']}</div>";
+<h3 class="text-center mt-4">
+Welcome <?= htmlspecialchars($_SESSION['user_name'] ?? 'User') ?>
+</h3>
+
+<hr class="w-25 mx-auto">
+<div class="card-box col-md-6 mx-auto">
+<input type="text" placeholder="Search assignments..." class="form-control mb-3">
+
+<?php
+$res = $conn->query("SELECT * FROM assignments");
+
+if($res->num_rows > 0){
+    while($row = $res->fetch_assoc()){
+        echo "<div class='p-2 border mb-2'>{$row['title']}</div>";
     }
 }else{
     echo "<p class='text-center'>No assignments found.</p>";
@@ -21,4 +31,6 @@ if($res->num_rows>0){
 
 </div>
 
-<?php include 'footer.php'; ?>
+<?php 
+include 'footer.php';
+?>
